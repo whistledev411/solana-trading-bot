cfa528bd1843644daae94bc5f6c6b2a74d2c0840
whistledev411 <antonisloannou46@gmail.com>
2024-01-13 00:23:15 -0500

    fix failing build

--- trader/providers/AutoTradeProvider.ts
+++ trader/providers/AutoTradeProvider.ts
@@ -3,25 +3,21 @@ import { LogProvider } from '@core/providers/LogProvider';
 import { TokenPriceProvider } from '@common/providers/TokenPriceProvider';
 import { TokenSwapProvider } from '@common/providers/TokenSwapProvider';
 import { TokenPriceObject, TokenPriceRequestPayload } from '@common/types/TokenPrice';
-import { ExecuteTradeProvider } from '@trader/providers/ExecuteTradeProvider';
 
 
 export class AutoTradeProvider {
-  private executeTradeProvider: ExecuteTradeProvider;
-
   constructor(
     private etcdProvider: ETCDProvider,
     private tokenPriceProvider: TokenPriceProvider,
     private tokenSwapProvider: TokenSwapProvider,
     private zLog: LogProvider = new LogProvider(AutoTradeProvider.name)
-  ) { /* this.executeTradeProvider = new ExecuteTradeProvider(this.etcdProvider, this.tokenSwapProvider); */ }
+  ) {}
 
   async start(request: TokenPriceObject<TokenPriceRequestPayload>) {
     this.tokenPriceProvider.startPriceListener('price_data', request);
     this.tokenPriceProvider.onPriceData('price_data', async priceData => {
       if (priceData.type !== 'WELCOME') {
         this.zLog.info(`price data: ${JSON.stringify(priceData, null, 2)}`);
-        // await this.executeTradeProvider.run(priceData.data.c);
       }
     });
   }
