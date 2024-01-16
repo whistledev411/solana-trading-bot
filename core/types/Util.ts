505f9b7aaa48ff30d4e91b3ef942c22ab3d631cc
whistledev411 <antonisloannou46@gmail.com>
2024-01-15 23:16:34 -0500

    fix inference utils

--- core/types/Util.ts
+++ core/types/Util.ts
@@ -1,24 +1,24 @@
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
-export type InferType<T = unknown, STRCT extends boolean = false, TYP = unknown>  = 
+export type InferType<T, STRCT, TYP = unknown>  = 
   T extends infer R 
-  ? R extends TYP ? R : TYP
+  ? R extends TYP 
+    ? R : TYP
   : STRCT extends true 
     ? never 
-    : T extends TYP ? T : TYP;
+    : T extends TYP 
+      ? T : TYP;
     
-type InferMappedType<MTYP, STRCT extends boolean = true> = { 
+type InferMappedType<MTYP, STRCT> = { 
   [K in keyof MTYP]: 
     MTYP[K] extends infer U 
-    ? InferTypeDeep<U, STRCT>
-    : MTYP[K];
+    ? InferTypeDeep<U, STRCT> 
+    : MTYP[K]
 }
 
 export type InferTypeDeep<T, STRCT = false, OPTNL = false> =
   T extends infer R 
-  ? (
-      OPTNL extends true 
-        ? MakeOptional<InferMappedType<R, STRCT extends boolean ? STRCT : never>, keyof R> 
-        : InferMappedType<R, STRCT extends boolean ? STRCT : never> 
-    ) 
-  : InferType<T, STRCT extends boolean ? STRCT : never>;
+  ? OPTNL extends true
+    ? MakeOptional<InferMappedType<R, STRCT>, keyof R>
+    : InferMappedType<R, STRCT>
+  : InferType<T, STRCT>;
