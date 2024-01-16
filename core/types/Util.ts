a2bb8f5ee9bf5b624d37718cdcf6776b3ec85c54
whistledev411 <antonisloannou46@gmail.com>
2024-01-15 23:01:11 -0500

    update conditonal types for dynamic data models

--- core/types/Util.ts
+++ core/types/Util.ts
@@ -5,21 +5,19 @@ export type InferType<T = unknown, STRCT extends boolean = false, TYP = unknown>
     ? R extends TYP ? R : TYP
     : STRCT extends true 
       ? never 
-      : T extends TYP ? T : TYP
+      : T extends TYP ? T : TYP;
     
-
-
-export type InferMappedType<MTYP, STRCT extends boolean = true> = { 
+type InferMappedType<MTYP, STRCT extends boolean = true> = { 
   [K in keyof MTYP]: MTYP[K] extends infer U 
     ? InferTypeDeep<U, STRCT>
     : MTYP[K];
 }
 
 export type InferTypeDeep<T, STRCT = false, OPTNL = false> =
-  T extends infer R
+  T extends infer R 
   ? (
       OPTNL extends true 
         ? MakeOptional<InferMappedType<R, STRCT extends boolean ? STRCT : never>, keyof R> 
         : InferMappedType<R, STRCT extends boolean ? STRCT : never> 
-    )
+    ) 
   : InferType<T, STRCT extends boolean ? STRCT : never>;
